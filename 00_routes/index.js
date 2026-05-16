7ed8b6f400a95e7380b5f043c1b0eecc9ee4b1ff
Varujan Ikilikyan <varujanna@gmail.com>
2026-05-16 20:05:30 +0400

    first route

--- 00_routes/index.js
+++ 00_routes/index.js
@@ -1,5 +1,7 @@
 import {Router} from "express";
 
+import views from "../02_controllers/views.js";
+
 const selectorRouter = new Router();
 
-selectorRouter.get('/',)
+selectorRouter.get('/',views.viewRender('index'));
